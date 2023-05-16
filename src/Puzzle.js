e894b9449c6413c092dc2a483ca13067e057a166
kumariritika12 <ritika133.1158@gmail.com>
2023-05-16 06:32:11 +0530

    questions updated

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -8,9 +8,9 @@ const Puzzle = () => {
 
   const questions = [
     {
-      question: 'What is the capital of France?',
-      options: ['New York', 'London', 'Paris', 'Dublin'],
-      correctAnswer: 'Paris'
+      question: 'If AIRLINE is written as ENILRIA7, then RAILWAY will be written as -',
+      options: ['YAWLIAR7', 'YAWLIAR8', 'YAWILAR7', 'YAWILAR8'],
+      correctAnswer: 'YAWLIAR7'
     },
     {
       question: 'Which continent is home to the largest rainforest in the world?',
@@ -18,25 +18,35 @@ const Puzzle = () => {
       correctAnswer: 'South America'
     },
     {
+      question: 'Monday : April : : Friday : ?',
+      options: ['July', 'Saturday', 'August', 'Tuesday'],
+      correctAnswer: 'August'
+    },
+    {
       question: 'What is the currency of Japan?',
       options: ['Yen', 'Euro', 'Dollar', 'Pound'],
       correctAnswer: 'Yen'
     },
     {
-      question: 'Who wrote the play "Romeo and Juliet"?',
-      options: ['William Shakespeare', 'Jane Austen', ' Charles Dickens', ' Mark Twain'],
-      correctAnswer: 'William Shakespeare'
+      question: 'In a group of five person A, B, C, D and E one plays Tennis, one plays Chess and one Hockey. A and D are unmarried women and· play no game. There is a couple among them where E is husband of C. No women plays either Chess or Hockey. B is the brother of C and he neither plays Tennis nor Chess. Who plays Hockey here?',
+      options: ['A', 'B', 'C', 'E'],
+      correctAnswer: 'B'
     },
     {
-      question: 'Who painted the famous artwork "Mona Lisa"?',
-      options: ['Leonardo da Vinci', 'Pablo Picasso', 'Vincent van Gogh', 'Michelangelo'],
-      correctAnswer: 'Leonardo da Vinci'
+      question: 'What will be the next number in the series 13, 17, 19, 23, 29, .......?',
+      options: ['31', '33', '35', '37'],
+      correctAnswer: '31'
     },
     {
       question: 'Who is CEO of Tesla?',
       options: ['Jeff Bezos', 'Elon Musk', 'Bill Gates', 'Tony Stark'],
       correctAnswer: 'Elon Musk'
-    }
+    },
+    {
+      question: 'A, Band C are intelligent, A, D and E are laborious and D, C and E are honest and . A, B, and E are ambitious. Which of the following are not honest?',
+      options: ['A and B', 'C and A', 'A and D', 'C, A and B'],
+      correctAnswer: 'A and B'
+    },
     // Add more questions as needed
   ];
 
@@ -92,7 +102,7 @@ const Puzzle = () => {
         return (
           <div>
             <h2>Congratulations!</h2>
-            <p>You answered all questions correctly!</p>
+            <p>You answered all questions correctly! You found the Treasure!!!!!</p>
             <button onClick={saveScore}>Save</button>
             <p>{saveMessage}</p>
           </div>
